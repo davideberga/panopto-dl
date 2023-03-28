69815e45acef6af2b5b8f99c9aa7d2512ed61fd2
Davide bergamasco <davideberga0@gmail.com>
2023-03-28 22:28:16 +0200

    add rich logging, bug fix

--- panopto-dl.py
+++ panopto-dl.py
@@ -1,4 +1,5 @@
 import argparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,13 +7,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import sys
-import socketserver
-import http.server
-import urllib.request
-from multiprocessing import Process
+from rich.progress import track
+import validators
 import selenium
 import time
+
+import logging
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("rich")
 
 parser = argparse.ArgumentParser(description='Download in batch videos from panopto')
 parser.add_argument('-url', metavar='--panopto-course-url', nargs=1,
@@ -27,48 +35,15 @@ url = args.url[0]
 username = args.u
 password = args.p
 
-PROXY_PORT = 8889
-PROXY_URL = 'localhost:%d' % PROXY_PORT
-
-class Proxy(http.server.SimpleHTTPRequestHandler):
-
-    def do_GET(self):
-        sys.stdout.write('%s → %s\n' % (self.headers.get('Referer', 'NO_REFERER'), self.path))
-        self.copyfile(urllib.request.urlopen(self.path), self.wfile)
-        sys.stdout.flush()
-
-    @classmethod
-    def target(cls):
-        httpd = socketserver.ThreadingTCPServer(('', PROXY_PORT), cls)
-        httpd.serve_forever()
-
-p_proxy = Process(target=Proxy.target)
-p_proxy.start()
-
-options = webdriver.ChromeOptions() ;
-prefs = {"download.default_directory" : "./downloads"};
-options.add_experimental_option("prefs",prefs);
-
-webdriver.DesiredCapabilities.CHROME['proxy'] = {
-    "httpProxy":PROXY_URL,
-    "ftpProxy":None,
-    "sslProxy":None,
-    "noProxy":None,
-    "proxyType":"MANUAL",
-    "class":"org.openqa.selenium.Proxy",
-    "autodetect":False
-}
-
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-browser.delete_all_cookies()
-browser.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Linux; Android 10; Generic Android-x86_64 Build/QD1A.190821.014.C2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/79.0.3945.36 Safari/537.36'})
+log.info("Start login to panopto...")
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 browser.get("https://univr.cloud.panopto.eu/Panopto/Pages/Auth/Login.aspx")
-
-# browser.maximize_window()
 
 idPanoptoSignIn = 'PageContentPlaceholder_loginControl_externalLoginButton'
 signInButton = browser.find_element(By.ID, idPanoptoSignIn)
 signInButton.click()
+
+log.info("Open login page with GIA credentials")
 
 videos = WebDriverWait(browser, 15).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".btn-login")))
 userInputLogin = browser.find_element(By.ID, 'form_username')
@@ -82,7 +57,8 @@ idSubmitLogin.click()
 time.sleep(5)
 
 browser.get(url)
-browser.maximize_window()
+log.info("Login succeded!")
+
 time.sleep(5)
 
 # WebDriverWait(browser, timeout=20000).until(lambda d: d.find_element(By.CLASS_NAME,"detail-title"))
@@ -94,43 +70,74 @@ for match in WebDriverWait(browser, 20).until(EC.presence_of_all_elements_locate
     except selenium.common.exceptions.NoSuchElementException as e:
         pass
 
-print(urlsVideos)
+if(len(urlsVideos.keys()) == 0):
+    log.error("0 videos found :(")
+else:
+    log.info(str(len(urlsVideos.keys())) + " videos found")
 
+log.info("Collecting pure video urls without login ... be patient")
 videos = dict()
-for video in urlsVideos.keys():
+for video in track(urlsVideos.keys(), description="Scraping..."):
     try:
         if urlsVideos[video] != None:
             browser.get(urlsVideos[video])
-            time.sleep(5)
+            time.sleep(2)
             primaryUrl = browser.find_element(By.ID, 'primaryVideo').get_attribute("src")
             secondaryUrl = browser.find_element(By.ID, 'secondaryVideo').get_attribute("src")
+
+            videos[video] = dict()
 
             videos[video]['primary'] = primaryUrl
             videos[video]['secondary'] = secondaryUrl
     except selenium.common.exceptions.NoSuchElementException as e:
         pass
 
-print(videos)
-
 import os
 
-def downloadfile(name,url):
+def downloadfile(name, url):
     name = name + ".mp4"
+
+    if not validators.url(url):
+        return False
+
     r = requests.get(url)
-    with open(name,'wb') as file:
-        for chunk in r.iter_content(chunk_size=255): 
-            if chunk: # filter out keep-alive new chunks
-                file.write(chunk)
+    if r.status_code >= 200 and r.status_code <= 210:
+        with open(name,'wb') as file:
+            for chunk in r.iter_content(chunk_size=255): 
+                if chunk: # filter out keep-alive new chunks
+                    file.write(chunk)
+    else:
+        log.error(url + " request error")
+        return False
+    log.info(name + " downloaded corectly!")
+    return True
 
 outputPath = "./panopto-lectures"
 if not os.path.exists(outputPath):
     os.makedirs(outputPath)
 
+# Create paths if not exists
 for video in videos:
     if not os.path.exists(os.path.join(outputPath, video)):
         os.makedirs(os.path.join(outputPath, video))
-    downloadfile(os.path.join(outputPath, video, "primary"), videos[video]['primary'])
-    downloadfile(os.path.join(outputPath, video, "secondary"), videos[video]['secondary'])
+
+log.info("Start downloading videos")
+with ThreadPoolExecutor(max_workers=4) as executor:
+
+    future_download = [executor.submit(downloadfile, os.path.join(outputPath, video, "primary"), videos[video]['primary']) for video in videos]
+    future_download += [executor.submit(downloadfile, os.path.join(outputPath, video, "secondary"), videos[video]['secondary']) for video in videos]
+    
+    for future in track(as_completed(future_download), description="Downloading..."):
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+
+log.info("[bold green blink]Done![/]", extra={ "markup" : True})
+
+
+
+
 
 
 
